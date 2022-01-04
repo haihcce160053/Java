ee3b3ff0fabf64b1c101cde55487a41e8d4b3899
Huỳnh Chí Hải <97091171+haihcce160053@users.noreply.github.com>
2022-01-04 19:58:38 +0700

    Create Xu_ly_chuoi.java

---
+++ Xu_ly_chuoi.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        String str = "Hello, my name is Hai"; //Chuỗi gốc      
+        
+        int dem_str = str.length(); //Đếm số lượng ký tự trong chuỗi str
+              
+        char[] ch = str.toCharArray(); //Thêm chuỗi vào char[]
+        
+        int dem_ch = ch.length; //Đếm ký tự trong char[]
+             
+        String[] split_str = str.split("\\s+"); //Tách chuỗi giữa các khoảng trắng
+      
+        System.exit(0); //Tắt chương trình
+    }
+}
