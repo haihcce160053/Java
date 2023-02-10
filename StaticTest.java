18a5bea196739628b6735fd7f4cad9c281582fe8
Huỳnh Chí Hải <97091171+haihcce160053@users.noreply.github.com>
2023-02-10 13:27:21 +0700

    Static_test

---
+++ StaticTest.java
@@ -0,0 +1,9 @@
+package com.mycompany.statictest;
+
+public class StaticTest {
+
+    public static void main(String[] args) {
+        String a;
+        System.out.println("Hello World!");
+    }
+}
